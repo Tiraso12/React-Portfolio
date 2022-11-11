5b87222f5c0f9d7c39e42f54c2d09ae7218c394a
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-11-10 19:17:05 -0500

    remove p

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,24 +2,23 @@ import React from "react";
 import './index.css'
 import photo from '../../assets/images/photo.jfif'
 
-import {Row, Col, Image } from "react-bootstrap";
+import { Row, Col, Image } from "react-bootstrap";
 
 export default function About() {
     return (
-            <Row className="tall">
-                <Col className="pt-5 pb-5  text-center align-self-center">
-                    <h1>I'm Gonzalo,</h1>
-                    <h3>Learning Web Development!!</h3>
-                    <p >lorem askdjsakdjsaj jd sopadpsa <br />djsapd ap djsapjdp sajjdpaspjpdaspj pdoajspjdasjodj <br />poasjdpojpsa jdpojaspjd pasjdp ojaspj<br /> dasjdj asj</p>
-                </Col>
-                <Col className="pt-5 pb-5  text-center align-self-center">
-                    <Image 
+        <Row className="tall">
+            <Col className="pt-5 pb-5  text-center align-self-center">
+                <h1>I'm Gonzalo,</h1>
+                <h3>Learning Web Development!!</h3>
+            </Col>
+            <Col className="pt-5 pb-5  text-center align-self-center">
+                <Image
                     src={photo}
-                    rounded/>
-                    
-                    
-                </Col>
-            </Row>
+                    rounded />
+
+
+            </Col>
+        </Row>
 
     )
 }
